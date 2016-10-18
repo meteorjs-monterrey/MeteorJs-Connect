65f96b480a168c080b8bc97c8b606492cd5ad5b2
rakirox <palmeroh@gmail.com>
2016-10-18 16:59:14 -0500

    finish

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,20 +4,13 @@ import { Messages } from '../api/messages.js'
 //~~no se agregan solos
 import './body.html'
 
-var msgsData      = Messages.find()
-
-// OLD AND UGLY
-// var onTextBoxEvent = function(event, template){
-//
-// }
-var onTextBoxEvent = (event, template) => {
-  if (event.which === 13) {
-    console.log("Meteor is Awesome");
-    console.log($('#textBox').val());
-    // ~~~~~~ woooooooot!? what's going on?
-    msgsData.push({text:template.$('#textBox').val()})
-  }
-}
+var msgsData       =  Messages.find(),
+    onTextBoxEvent = (event, template) => {
+      if (event.which === 13) {
+        Messages.insert({text:template.$('#textBox').val()})
+        $('#textBox').val('')
+      }
+    }
 
 
 var bodyHelpers   = {  messages: msgsData },
--- server/main.js
+++ server/main.js
@@ -2,5 +2,4 @@ import { Meteor } from 'meteor/meteor';
 import { Messages } from '../imports/api/messages.js';
 Meteor.startup(() => {
   // code to run on server at startup
-  Messages.insert({text:"Hey I'm from collection"});
 });
