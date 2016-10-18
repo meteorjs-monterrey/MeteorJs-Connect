3ace6ac3f6ad0f0af27d76537c959f41df216388
rakirox <palmeroh@gmail.com>
2016-10-18 16:01:12 -0500

    changes

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,7 +4,24 @@ import { Messages } from '../api/messages.js'
 //~~no se agregan solos
 import './body.html'
 
-var msgsData      = Messages.find(),
-    bodyHelpers   = {  messages: msgsData }
+var msgsData      = Messages.find()
+
+// OLD AND UGLY
+// var onTextBoxEvent = function(event, template){
+//
+// }
+var onTextBoxEvent = (event, template) => {
+  if (event.which === 13) {
+    console.log("Meteor is Awesome");
+    console.log($('#textBox').val());
+    // ~~~~~~ woooooooot!? what's going on?
+    msgsData.push({text:template.$('#textBox').val()})
+  }
+}
+
+
+var bodyHelpers   = {  messages: msgsData },
+    bodyEvents    = { "keypress #textBox" : onTextBoxEvent }
 
 Template.body.helpers(bodyHelpers)
+Template.body.events(bodyEvents);
