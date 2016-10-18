1013876fbf0dd95f965652ce88c82ea667c65368
rakirox <palmeroh@gmail.com>
2016-10-18 02:15:34 -0500

    collection added

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,12 +2,10 @@ import { Template } from 'meteor/templating'
 //Agregamos nuestro template porque los archivos dentro de 'imports' ~~
 //~~no se agregan solos
 import './body.html'
+import '../imports/api/messages.js'
 
-var msgsData      = [
-    { text: 'Hello MeteorJs!' },
-    { text: 'yep, this is a Js world' },
-    { text: 'we are learning MeteorJs' }
-  ]
+var msgsData      = Messages.find()
+
 
 var bodyHelpers   = {  messages: msgsData }
 
